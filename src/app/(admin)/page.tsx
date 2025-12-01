a56dd249ef0de2f48c8b65f7a15b07633368880d
beki55 <bereket21bnm@gmail.com>
2025-12-01 09:46:21 +0300

    Some fixes

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -9,8 +9,8 @@ import DemographicCard from "@/components/ecommerce/DemographicCard";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+    "BDU Blog Home | BDU Blog",
+  description: "This is for BDU Blog Dashboard Home Page",
 };
 
 export default function Ecommerce() {
